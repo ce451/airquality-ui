118bd57b55d69f7f01648cabf487608df91e4c57
Christopher Elstner <72193429+ce451@users.noreply.github.com>
2025-08-08 09:10:35 +0200

    station card chart optimizations

--- src/app/shared/components/station-card/station-card.ts
+++ src/app/shared/components/station-card/station-card.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {Station} from 'src/app/core/models/station.model';
 import {StationService} from 'src/app/core/services/station.service';
 import {ChartConfiguration, ChartType} from 'chart.js';
 import {Measurement} from 'src/app/core/models/measurement.model';
+import {BaseChartDirective} from 'ng2-charts';
 
 @Component({
   selector: 'app-station-card',
@@ -12,57 +13,26 @@ import {Measurement} from 'src/app/core/models/measurement.model';
 })
 export class StationCard {
   @Input() station!: Station;
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   tempColor = this.getCssVar('--color-temperature');
   humColor = this.getCssVar('--color-humidity');
   absHumColor = this.getCssVar('--color-absolute-humidity');
 
+  minTemp = 0;
+  maxTemp = 50;
+  minHum = 0;
+  maxHum = 100;
+  minAbsHum = 0;
+  maxAbsHum = 30;
 
   chartData!: ChartConfiguration['data'];
-  chartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    // maintainAspectRatio: false,
-    scales: {
-      x: {
-        display: false,
-        reverse: true
-      },
-      y: {
-        beginAtZero: true,
-        ticks: {
-          color: this.tempColor,
-        }
-      },
-      y1: {
-        beginAtZero: true,
-        ticks: {
-          color: this.humColor,
-        }
-      },
-      y2: {
-        beginAtZero: true,
-        ticks: {
-          color: this.absHumColor,
-        }
-      }
-    },
-    elements: {
-      point: {
-        pointStyle: false
-      }
-    },
-    plugins: {
-      legend: {
-        display: false
-      }
-    }
-  };
+  chartOptions!: ChartConfiguration['options'];
   chartType: ChartType = 'line';
 
   constructor(private stationService: StationService) {
   }
 
-  // onInit
   ngOnInit() {
     if (!this.station) {
       throw new Error('Station input is required');
@@ -74,6 +44,8 @@ export class StationCard {
         console.log('measurements for station:', data);
         if(data.measurements) {
           this.parseMeasurements(data.measurements);
+          this.setupChartOptions();
+          this.chart?.update();
         }
       });
   }
@@ -84,6 +56,23 @@ export class StationCard {
     const humidityData = measurements.map(m => m.humidity);
     const absoluteHumidityData = measurements.map(m => m.absoluteHumidity);
 
+    this.minTemp = Math.floor(Math.min(...temperatureData)) - 2;
+    this.maxTemp = Math.ceil(Math.max(...temperatureData)) + 2;
+    this.minHum = Math.floor(Math.min(...humidityData) / 10) * 10 - 10;
+    this.maxHum = Math.ceil(Math.max(...humidityData)) + 2;
+    this.minAbsHum = Math.floor(Math.min(...absoluteHumidityData)) - 1;
+    this.maxAbsHum = Math.ceil(Math.max(...absoluteHumidityData)) + 5;
+
+    // log all the min and max values
+    console.log('Min/Max Values:', {
+      minTemp: this.minTemp,
+      maxTemp: this.maxTemp,
+      minHum: this.minHum,
+      maxHum: this.maxHum,
+      minAbsHum: this.minAbsHum,
+      maxAbsHum: this.maxAbsHum
+    });
+
     this.chartData = {
       labels: labels,
       datasets: [
@@ -92,23 +81,88 @@ export class StationCard {
           data: temperatureData,
           borderColor: this.tempColor,
           backgroundColor: this.tempColor + '33',
-          fill: true
+          fill: true,
+          order: 1,
+          yAxisID: 'y',
+          tension: 0.5,
+          cubicInterpolationMode: 'monotone',
         },
         {
           label: 'Humidity (%)',
           data: humidityData,
           borderColor: this.humColor,
           backgroundColor: this.humColor + '33',
-          fill: true
+          fill: true,
+          order: 2,
+          yAxisID: 'y1',
+          tension: 0.4,
+          cubicInterpolationMode: 'monotone',
         },
         {
           label: 'Absolute Humidity (g/m³)',
           data: absoluteHumidityData,
           borderColor: this.absHumColor,
           backgroundColor: this.absHumColor + '33',
-          fill: true
+          fill: true,
+          order: 3,
+          yAxisID: 'y2',
+          tension: 0.4,
+          cubicInterpolationMode: 'monotone',
         }
       ]
+    };
+  }
+
+  setupChartOptions() {
+    this.chartOptions = {
+      responsive: true,
+      // maintainAspectRatio: false,
+      scales: {
+        x: {
+          display: false,
+          reverse: true
+        },
+        y: {
+          beginAtZero: false,
+          ticks: {
+            color: this.tempColor,
+          },
+          grid: {
+            drawOnChartArea: false,
+          },
+          min: this.minTemp,
+          max: this.maxTemp,
+        },
+        y1: {
+          beginAtZero: false,
+          ticks: {
+            color: this.humColor,
+          },
+          grid: {
+            drawOnChartArea: false,
+          },
+          min: this.minHum,
+          max: this.maxHum,
+        },
+        y2: {
+          beginAtZero: false,
+          ticks: {
+            color: this.absHumColor,
+          },
+          min: this.minAbsHum,
+          max: this.maxAbsHum,
+        }
+      },
+      elements: {
+        point: {
+          pointStyle: false
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
     };
   }
 
